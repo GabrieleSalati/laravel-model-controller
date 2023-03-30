f0deeb2ec0b670a523a211beeabbdedea1d378ff
GabrieleSalati <gabriele.salati5@gmail.com>
2023-03-30 17:15:13 +0200

    Database call successful

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\movie;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view("home");
+        $movies = movie::all();
+        dd($movies);
+        return view("home", compact("movies"));
     }
 }
